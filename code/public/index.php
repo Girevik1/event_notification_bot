5736badc50ea2a076df3969786c281d1f5396f14
Artur <se6@rep.earth>
2023-10-11 13:03:19 +0300

    test nginx

--- code/public/index.php
+++ code/public/index.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 require '../vendor/autoload.php';
 
 try {
-    echo 111111111;
-    (new \Art\Code\Application\Helper\DotEnv(__DIR__ . '/../.env'))->load();
-    (new \Art\Code\Infrastructure\Http\Controllers\BotController)->run();
+    echo phpinfo();
+//    (new \Art\Code\Application\Helper\DotEnv(__DIR__ . '/../.env'))->load();
+//    (new \Art\Code\Infrastructure\Http\Controllers\BotController)->run();
 } catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL;
 }
