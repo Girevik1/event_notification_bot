2fa200ffe64f3ab16f71aea902c00cdd71a40b04
Artur <se6@rep.earth>
2023-10-23 15:26:42 +0300

    fix create group in bd

--- code/src/Application/UseCase/Bot/GroupUseCase.php
+++ code/src/Application/UseCase/Bot/GroupUseCase.php
@@ -9,6 +9,7 @@ use Art\Code\Domain\Entity\TelegramUser;
 use Art\Code\Domain\Exception\GroupCreateException;
 use Art\Code\Domain\Exception\GroupDeleteException;
 use Art\Code\Infrastructure\Repository\TelegramGroupRepository;
+use Illuminate\Support\Collection;
 use Telegram\Bot\Api;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 
@@ -20,43 +21,43 @@ class GroupUseCase
      * @throws GroupDeleteException
      */
     public function groupHandlerByMessage(
-//        array                   $message,
+        Collection                   $message,
         TelegramGroupRepository $groupRepository,
         Api                     $telegram,
         TextUseCase             $textUseCase,
         TelegramUser $user
     ): bool
     {
-        $telegram->sendMessage([
-            'chat_id' => -1001743972342,
-            'parse_mode' => 'HTML',
-            'text' => 'testest'
-        ]);//
-
-//        if (isset($message['left_chat_member'])) {
-//            $resulDelete = $groupRepository->deleteByChatId($message['chat']['id']);
-//            if (!$resulDelete) {
-//                throw new GroupDeleteException('Не удалось удалить группу с БД');
-//            }
-//            // TODO удалить все эвенты связанные с этой группой
-//            return true;
-//        }
-//
-//        $telegramGroupDto = new TelegramGroupDto($message);
-//
-//        if (!$groupRepository->create($telegramGroupDto)) {
-//            throw new GroupCreateException('Ошибка добавления группы в БД');
-//        }
-//
-//        $message = $textUseCase->getGreetingsGroupText($user);
-//
 //        $telegram->sendMessage([
-////            'chat_id' => -1001743972342,
-//            'chat_id' => $telegramGroupDto->group_chat_id,
+//            'chat_id' => -1001743972342,
 //            'parse_mode' => 'HTML',
-//            'text' => $message
-//        ]);
-//
-//        return true;
+//            'text' => 'testest'
+//        ]);//
+
+        if (isset($message['left_chat_member'])) {
+            $resulDelete = $groupRepository->deleteByChatId($message['chat']['id']);
+            if (!$resulDelete) {
+                throw new GroupDeleteException('Не удалось удалить группу с БД');
+            }
+            // TODO удалить все эвенты связанные с этой группой
+            return true;
+        }
+
+        $telegramGroupDto = new TelegramGroupDto($message);
+
+        if (!$groupRepository->create($telegramGroupDto)) {
+            throw new GroupCreateException('Ошибка добавления группы в БД');
+        }
+
+        $message = $textUseCase->getGreetingsGroupText($user);
+
+        $telegram->sendMessage([
+//            'chat_id' => -1001743972342,
+            'chat_id' => $telegramGroupDto->group_chat_id,
+            'parse_mode' => 'HTML',
+            'text' => $message
+        ]);
+
+        return true;
     }
 }
