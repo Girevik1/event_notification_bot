64b694f2f212925267daf0432ea83d5fa6c80432
Artur <se6@rep.earth>
2023-10-31 00:24:34 +0300

    Check get event list

--- code/src/Application/UseCase/Bot/TextUseCase.php
+++ code/src/Application/UseCase/Bot/TextUseCase.php
@@ -67,7 +67,7 @@ class TextUseCase
                 $dateOfEvent = Carbon::parse($event->date_event_at)->format('d.m.Y');
 
                 $text .= "<b>" . $key + 1 . ".</b> " . $eventName . "\n";
-//                $text .= " Имя: <i>" . $event->name . "</i>\n";
+                $text .= " Имя: <i>" . $event->name . "</i>\n";
 //                $text .= " Дата: <i>" .  $dateOfEvent . "</i>\n\n";
             }
 //            $text .= "<b>Для удаления события отправьте номер записи<b>";
