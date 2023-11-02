a048a5eceb3d0a5d91001d369878cd1445787d6b
Artur <se6@rep.earth>
2023-11-02 14:29:06 +0300

    check cron birthday method

--- code/src/Application/UseCase/Bot/BotUseCase.php
+++ code/src/Application/UseCase/Bot/BotUseCase.php
@@ -488,28 +488,35 @@ final class BotUseCase
     public function checkBirthdayToday(){
         $now = Carbon::now()->addHours(3);
 
-        $listBirthdayEvents = ListEvent::where('type', 'birthday')
-            ->whereMonth('date_event_at', $now->format('m'))
-            ->whereDay('date_event_at', $now->format('d'))
-            ->where('notification_time_at', $now->format('h:i'))
-            ->get();
+//        $listBirthdayEvents = ListEvent::where('type', 'birthday')
+//            ->whereMonth('date_event_at', $now->format('m'))
+//            ->whereDay('date_event_at', $now->format('d'))
+//            ->where('notification_time_at', $now->format('h:i'))
+//            ->get();
+//
+//        foreach ($listBirthdayEvents as $event) {
+//
+//            $telegramUser = $this->telegramUserRepository->firstById($event->telegram_user_id);
+//
+//            $dateOfBirth = Carbon::parse($event->date_event_at);
+//            $diffYears = $dateOfBirth->diffInYears($now);
+//            $correctFormat = $this->yearTextArg($diffYears);
+//
+//            $messageSendDto = new MessageSendDto();
+//            $messageSendDto->text = "<b>Сегодня день рождение</b>!";
+//            $messageSendDto->text .= "\n\n" . $event->name . " <b>" . $diffYears . " " . $correctFormat . "</b>!";
+//            $messageSendDto->user = $telegramUser;
+//            $messageSendDto->command = 'cron_birthday';
+//
+//            TelegramMessage::newMessage($messageSendDto);
+//        }
+        $telegramUser = $this->telegramUserRepository->firstById(4);
+        $messageSendDto = new MessageSendDto();
+        $messageSendDto->text = "wewe!";
+        $messageSendDto->user = $telegramUser;
+        $messageSendDto->command = 'cron_birthday';
 
-        foreach ($listBirthdayEvents as $event) {
-
-            $telegramUser = $this->telegramUserRepository->firstById($event->telegram_user_id);
-
-            $dateOfBirth = Carbon::parse($event->date_event_at);
-            $diffYears = $dateOfBirth->diffInYears($now);
-            $correctFormat = $this->yearTextArg($diffYears);
-
-            $messageSendDto = new MessageSendDto();
-            $messageSendDto->text = "<b>Сегодня день рождение</b>!";
-            $messageSendDto->text .= "\n\n" . $event->name . " <b>" . $diffYears . " " . $correctFormat . "</b>!";
-            $messageSendDto->user = $telegramUser;
-            $messageSendDto->command = 'cron_birthday';
-
-            TelegramMessage::newMessage($messageSendDto);
-        }
+        TelegramMessage::newMessage($messageSendDto);
     }
 
     private function yearTextArg($year)
