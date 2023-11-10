3981aacea1cba21451676faef2013893819abf15
Artur <se6@rep.earth>
2023-11-11 00:55:56 +0300

    check too long msg

--- code/src/Infrastructure/Telegram/TelegramHandler.php
+++ code/src/Infrastructure/Telegram/TelegramHandler.php
@@ -65,8 +65,8 @@ final class TelegramHandler implements TelegramHandlerInterface
             $textArray = [];
             $start = 0;
             do {
-                $textArray[] = mb_strcut($text, $start, 17000);
-                $start += 17000;
+                $textArray[] = mb_strcut($text, $start, 16000);
+                $start += 16000;
             } while (mb_strlen($text, '8bit') > $start);
 //        $textArray[] = (string)mb_strlen($text, '8bit');
 
