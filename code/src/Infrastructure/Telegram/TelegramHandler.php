d7752e3e2523a523a2bd57c83b1fac960d41dac3
Artur <se6@rep.earth>
2023-11-11 00:17:47 +0300

    check too long msg

--- code/src/Infrastructure/Telegram/TelegramHandler.php
+++ code/src/Infrastructure/Telegram/TelegramHandler.php
@@ -63,12 +63,12 @@ final class TelegramHandler implements TelegramHandlerInterface
     private function newMessage($text, $chatId, $typeBtn, $telegramMessageRepository): void
     {
             $textArray = [];
-            $start = 0;
-            do {
-                $textArray[] = mb_strcut($text, $start, 4096);
-                $start += 4096;
-            } while (mb_strlen($text, '8bit') > $start);
-        $textArray = $textArray[0];
+//            $start = 0;
+//            do {
+//                $textArray[] = mb_strcut($text, $start, 4096);
+//                $start += 4096;
+//            } while (mb_strlen($text, '8bit') > $start);
+        $textArray[] = $text;
 
         foreach ($textArray as $textItem) {
             $typeBtnForLastMsg = '';
