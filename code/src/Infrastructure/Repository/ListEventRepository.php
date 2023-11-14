f5ac27c90d12142f53b85e1d582b55697b11ee5c
Artur <se6@rep.earth>
2023-11-14 22:42:24 +0300

    added anniversary

--- code/src/Infrastructure/Repository/ListEventRepository.php
+++ code/src/Infrastructure/Repository/ListEventRepository.php
@@ -35,7 +35,7 @@ class ListEventRepository implements ListEventRepositoryInterface
     public function getListByUser(int $userId): Collection
     {
         return ListEvent::where('telegram_user_id', '=', $userId)
-            ->orderBy('id','desc')
+            ->orderBy('id','asc')
             ->get();
     }
 
